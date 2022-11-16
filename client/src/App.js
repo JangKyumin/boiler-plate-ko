cf68818263d2ab66173b9a8489743bc23565b3c4
kmjang <kyumin.jang@gmail.com>
2022-11-16 22:38:50 +0900

    오류 처리

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="" element={<LandingPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/login" element={<LoginPage />} />
+          <Route exact path="/register" element={<RegisterPage />} />
         </Routes>
       </div>
     </Router>
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,41 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Auth from "../../../hoc/auth";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
-    axios.get("/api/hello").then((res) => console.log(res.data));
+    axios.get("/api/hello").then((response) => {
+      console.log(response);
+    });
   }, []);
-  return <div>LandingPage Test</div>;
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/login");
+      } else {
+        alert("로그아웃 하는데 실패 했습니다.");
+      }
+    });
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
+    </div>
+  );
 }
 
-export default LandingPage;
+export default Auth(LandingPage, null);
